ca770c02504a757afbc35f5277f0e57bccb78110
Giorgio Piatti <contact@giorgiopiatti.ch>
2021-04-12 16:30:44 +0200

    update(example)

--- examples/example.py
+++ examples/example.py
@@ -30,3 +30,10 @@ except PPLTypeError as ex:
 # FunctionCallSignature: [<class 'int'>, arg1: <class 'int'>],
 # FunctionCallSignature: [<class 'int'>, arg1: <class 'bool'>])
 t.prorogued_method1(42, arg1=True)
+
+
+res = t.prorogued_method1(42, arg1=10)  # TYPING: ok. Uses cached value
+
+# When res is str, raise error. Otherwise continues execution. This is the normal Python behaviour
+print(res + 3)
+res.invalid_property  # raised always a error
