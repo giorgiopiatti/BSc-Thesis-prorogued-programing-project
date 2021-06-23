41aaff2bb6b4881db9a1b3ee6b8a9e40009c92d4
Giorgio Piatti <contact@giorgiopiatti.ch>
2021-06-23 14:21:24 +0200

    fix(package setup)

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 setup(
-    name='Prorogued Programmin Language - Python library',
-    packages=find_packages(include=['ppl']),
-    version='0.0.0',
+    name='ppl',
+    packages=['ppl'],
+    version='0.0.1',
     description='',
     author='Giorgio Piatti',
     license='',
-    install_requires=['lark-parser==0.11.2', 'cachetools==4.2.2'],
+    install_requires=['lark-parser==0.11.2', 'cachetools==4.2.2', 'tk==0.1.0'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
