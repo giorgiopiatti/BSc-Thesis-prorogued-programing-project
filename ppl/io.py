387461a2e6b97d966b1df21e5568d3507a1f980d
Giorgio Piatti <contact@giorgiopiatti.ch>
2021-06-23 14:22:12 +0200

    update(UI now sidebar)

--- ppl/io.py
+++ ppl/io.py
@@ -2,35 +2,37 @@ import tkinter as tk
 import sys
 
 
+SIZE = 35
+
+
 class ExampleApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title('PPL')
-        frame = tk.Frame(self, width=800)
-        frame.pack(side="top",  fill='x')
-        self.text = tk.Text(self, wrap="word")
-        self.text.pack(side="left", pady=5, padx=5, fill='both', expand=True)
+        self.geometry("500x1200")
+        self.text = tk.Text(self, wrap="word", width=SIZE)
+        self.text.place(relheight=.45)
+        #self.text.pack(side="top", pady=5, padx=5, expand=True)
         self.text.tag_configure("stderr", foreground="#b22222")
 
-        self.text_instance = tk.Text(self, wrap="word")
-        self.text_instance.pack(
-            side="top", pady=5, padx='5', fill='both', expand=True)
+        self.text_instance = tk.Text(self, wrap="word",  width=SIZE)
+        self.text_instance.place(relheight=.45, rely=.45)
 
         self.text_instance.tag_configure("stderr", foreground="#b22222")
 
-        self.entry = tk.Entry(self)
+        self.entry = tk.Entry(self,  width=SIZE)
         self.msg = tk.Label(self, text='')
 
-        self.msg.pack(side="top")
-        self.entry.pack(side="bottom", fill="both",
-                        expand=True,  pady=5, padx=5)
-
+        
+        self.msg.place(relheight=.05, rely=.90)
+        self.entry.place(relheight=.05, rely=.95)
 
 app = ExampleApp()
 
 
 def write(s):
     app.text.configure(state="normal")
+    app.text.delete(1.0, tk.END)
     app.text.insert("end", f'{s}\n', ('stdout', ))
     app.text.configure(state="disabled")
 
